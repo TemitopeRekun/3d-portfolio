ffea2ddf0ea7a7e9ab21111eb79962918fec4130
Temitope Ogunrekun <olalekanogunrekun@gmail.com>
2024-06-25 22:16:07 +0100

    styling the project component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+	index,
+	name,
+	description,
+	tags,
+	image,
+	source_code_link,
+}) => {
+	return (
+		<motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+			<Tilt
+				options={{
+					max: 45,
+					scale: 1,
+					speed: 450,
+				}}
+				className="bg-tertiary p-5 rounded-2xl sm:w-[360px]">
+				<div className="relative w-full h-[230px]">
+					<img
+						src={image}
+						alt="name"
+						className="w-full h-full object-cover rounded-2xl"
+					/>
+					<div className="absolute inset-0 flex justify-end m-3 card-img_hover ">
+						<div
+							onClick={() => window.open(source_code_link, "_blank")}
+							className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+							<img
+								src={github}
+								alt="github"
+								className="w-1/2 h-1/2 object-contain"
+							/>
+						</div>
+					</div>
+				</div>
+			</Tilt>
+		</motion.div>
+	);
+};
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}> My Work</p>
+				<h2 className={styles.sectionHeadText}>Projects.</h2>
+			</motion.div>
 
-export default Works
+			<div className="w-full flex">
+				<motion.p
+					variants={fadeIn("", "", 0.1, 1)}
+					className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+					The projects below highlight my technical skills and experience
+					through real-world applications. Each project includes a brief
+					overview, along with links to the code repositories and live
+					demos. These examples demonstrate my expertise in solving complex
+					problems, utilizing diverse technologies, and effectively
+					managing projects.
+				</motion.p>
+			</div>
+
+			<div className="mt-20 flex flex-wrap gap-7">
+				{projects.map((project, index) => (
+					<ProjectCard
+						key={`project-${index}`}
+						{...project}
+						index={index}
+					/>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default SectionWrapper(Works, "");
