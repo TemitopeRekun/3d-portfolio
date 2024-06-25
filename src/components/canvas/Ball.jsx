1cb2a206b310b584f486b9032b81593fb2136e67
Temitope Ogunrekun <olalekanogunrekun@gmail.com>
2024-06-25 21:23:49 +0100

    finished the tech section

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,33 +1,50 @@
-import React, { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
-import CanvasLoader from "../Loader"
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+	Decal,
+	Float,
+	OrbitControls,
+	Preload,
+	useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl])
-  
-  return (
-    <Float speed={1.5} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.25]} />
-    </Float>
-  )
-}
+	const [decal] = useTexture([props.imgUrl]);
+
+	return (
+		<Float speed={1.5} rotationIntensity={1} floatIntensity={2}>
+			<ambientLight intensity={0.25} />
+			<directionalLight position={[0, 0, 0.25]} />
+			<mesh castShadow receiveShadow scale={2.75}>
+				<icosahedronGeometry args={[1, 1]} />
+				<meshStandardMaterial
+					color="#fff8eb"
+					polygonOffset
+					polygonOffsetFactor={-5}
+				/>
+				<Decal
+					position={[0, 0, 1]}
+					map={decal}
+					rotation={[2 * Math.PI, 0, 6.25]}
+					floatshading
+				/>
+			</mesh>
+		</Float>
+	);
+};
 
 const BallCanvas = ({ icon }) => {
-  <Canvas
-    frameloop="demand"
-			gl={{ preserveDrawingBuffer: true }}>
+	return (
+		<Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
 			<Suspense fallback={<CanvasLoader />}>
-				<OrbitControls
-					enableZoom={false}
-				/>
-      <Ball imgUrl={icon} />
+				<OrbitControls enableZoom={false} />
+				<Ball imgUrl={icon} />
 			</Suspense>
 
 			<Preload all />
-  </Canvas>
-
-}
+		</Canvas>
+	);
+};
 
 export default BallCanvas;
